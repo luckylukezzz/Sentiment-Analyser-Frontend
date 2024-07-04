0062cd361306a42ccbbf758609b2056d4382b6e1
patalee narasinghe <patalee2001@gmail.com>
2024-07-04 19:39:00 +0530

    deleted eslint

--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
-import { FiSettings } from 'react-icons/fi';
-import { TooltipComponent } from '@syncfusion/ej2-react-popups';
-import { Navbar, Sidebar,Footer ,ThemeSettings } from './components';
-import Landing from './pages/Landing';
-import Register from './pages/Register';
-import Logout from './pages/Logout';
-import Login from './pages/Login';
-import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
-import PrivateRoute from './components/PrivateRoute';
-import './App.css';
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { FiSettings } from "react-icons/fi";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import { Navbar, Sidebar, Footer, ThemeSettings } from "./components";
+import Landing from "./pages/Landing";
+import Register from "./pages/Register";
+import Logout from "./pages/Logout";
+import Login from "./pages/Login";
+import {
+  Ecommerce,
+  Orders,
+  Calendar,
+  Employees,
+  Stacked,
+  Pyramid,
+  Customers,
+  Kanban,
+  Line,
+  Area,
+  Bar,
+  Pie,
+  Financial,
+  ColorPicker,
+  ColorMapping,
+  Editor,
+} from "./pages";
+import PrivateRoute from "./components/PrivateRoute";
+import "./App.css";
 
-
-import { useStateContext } from './contexts/ContextProvider';
+import { useStateContext } from "./contexts/ContextProvider";
 
 const App = () => {
-  
-  
-  const { setCurrentColor, setCurrentMode, currentMode, activeMenu, currentColor, themeSettings, setThemeSettings } = useStateContext();
+  const {
+    setCurrentColor,
+    setCurrentMode,
+    currentMode,
+    activeMenu,
+    currentColor,
+    themeSettings,
+    setThemeSettings,
+  } = useStateContext();
   const location = useLocation();
 
   useEffect(() => {
-    const currentThemeColor = localStorage.getItem('colorMode');
-    const currentThemeMode = localStorage.getItem('themeMode');
+    const currentThemeColor = localStorage.getItem("colorMode");
+    const currentThemeMode = localStorage.getItem("themeMode");
     if (currentThemeColor && currentThemeMode) {
       setCurrentColor(currentThemeColor);
       setCurrentMode(currentThemeMode);
@@ -30,19 +52,19 @@ const App = () => {
   }, []);
 
   const isDashboardRoute = () => {
-    return location.pathname.startsWith('/dashboard');
+    return location.pathname.startsWith("/dashboard");
   };
 
   return (
-    <div className={currentMode === 'Dark' ? 'dark' : ''}>
+    <div className={currentMode === "Dark" ? "dark" : ""}>
       <div className="flex relative dark:bg-main-dark-bg">
         {isDashboardRoute() && (
-          <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
+          <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
             <TooltipComponent content="Settings" position="Top">
               <button
                 type="button"
                 onClick={() => setThemeSettings(true)}
-                style={{ background: currentColor, borderRadius: '50%' }}
+                style={{ background: currentColor, borderRadius: "50%" }}
                 className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
               >
                 <FiSettings />
@@ -50,8 +72,8 @@ const App = () => {
             </TooltipComponent>
           </div>
         )}
-        {isDashboardRoute() && (
-          activeMenu ? (
+        {isDashboardRoute() &&
+          (activeMenu ? (
             <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
               <Sidebar />
             </div>
@@ -59,13 +81,12 @@ const App = () => {
             <div className="w-0 dark:bg-secondary-dark-bg">
               <Sidebar />
             </div>
-          )
-        )}
+          ))}
         <div
           className={
             activeMenu && isDashboardRoute()
-              ? 'dark:bg-main-dark-bg bg-main-bg min-h-screen md:ml-72 w-full'
-              : 'bg-main-bg dark:bg-main-dark-bg w-full min-h-screen flex-2'
+              ? "dark:bg-main-dark-bg bg-main-bg min-h-screen md:ml-72 w-full"
+              : "bg-main-bg dark:bg-main-dark-bg w-full min-h-screen flex-2"
           }
         >
           {isDashboardRoute() && (
@@ -74,44 +95,93 @@ const App = () => {
             </div>
           )}
           <div>
-          {themeSettings && (<ThemeSettings />)}
+            {themeSettings && <ThemeSettings />}
 
             <Routes>
-       
               {/* landing/login/logout */}
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
               <Route path="/logout" element={<Logout />} />
               <Route path="/" element={<Landing />} />
               {/* dashboard */}
-              <Route path="/dashboard/" element={<PrivateRoute element={Ecommerce} />} />
-              <Route path="/dashboard/ecommerce" element={<PrivateRoute element={Ecommerce} />} />
-                 
-               {/* pages */}
-              <Route path="/dashboard/orders" element={<PrivateRoute element={Orders} />} />
-              <Route path="/dashboard/employees" element={<PrivateRoute element={Employees} />} />
-              <Route path="/dashboard/customers" element={<PrivateRoute element={Customers} />} />
+              <Route
+                path="/dashboard/"
+                element={<PrivateRoute element={Ecommerce} />}
+              />
+              <Route
+                path="/dashboard/ecommerce"
+                element={<PrivateRoute element={Ecommerce} />}
+              />
+
+              {/* pages */}
+              <Route
+                path="/dashboard/orders"
+                element={<PrivateRoute element={Orders} />}
+              />
+              <Route
+                path="/dashboard/employees"
+                element={<PrivateRoute element={Employees} />}
+              />
+              <Route
+                path="/dashboard/customers"
+                element={<PrivateRoute element={Customers} />}
+              />
 
               {/* apps */}
-              <Route path="/dashboard/kanban" element={<PrivateRoute element={Kanban} />} />
-              <Route path="/dashboard/editor" element={<PrivateRoute element={Editor} />} />
-              <Route path="/dashboard/calendar" element={<PrivateRoute element={Calendar} />} />
-              <Route path="/dashboard/color-picker" element={<PrivateRoute element={ColorPicker} />} />
+              <Route
+                path="/dashboard/kanban"
+                element={<PrivateRoute element={Kanban} />}
+              />
+              <Route
+                path="/dashboard/editor"
+                element={<PrivateRoute element={Editor} />}
+              />
+              <Route
+                path="/dashboard/calendar"
+                element={<PrivateRoute element={Calendar} />}
+              />
+              <Route
+                path="/dashboard/color-picker"
+                element={<PrivateRoute element={ColorPicker} />}
+              />
 
               {/* charts */}
-              <Route path="/dashboard/line" element={<PrivateRoute element={Line} />} />
-              <Route path="/dashboard/area" element={<PrivateRoute element={Area} />} />
-              <Route path="/dashboard/bar" element={<PrivateRoute element={Bar} />} />
-              <Route path="/dashboard/pie" element={<PrivateRoute element={Pie} />} />
-              <Route path="/dashboard/financial" element={<PrivateRoute element={Financial} />} />
-              <Route path="/dashboard/color-mapping" element={<PrivateRoute element={ColorMapping} />} />
-              <Route path="/dashboard/pyramid" element={<PrivateRoute element={Pyramid} />} />
-              <Route path="/dashboard/stacked" element={<PrivateRoute element={Stacked} />} />
+              <Route
+                path="/dashboard/line"
+                element={<PrivateRoute element={Line} />}
+              />
+              <Route
+                path="/dashboard/area"
+                element={<PrivateRoute element={Area} />}
+              />
+              <Route
+                path="/dashboard/bar"
+                element={<PrivateRoute element={Bar} />}
+              />
+              <Route
+                path="/dashboard/pie"
+                element={<PrivateRoute element={Pie} />}
+              />
+              <Route
+                path="/dashboard/financial"
+                element={<PrivateRoute element={Financial} />}
+              />
+              <Route
+                path="/dashboard/color-mapping"
+                element={<PrivateRoute element={ColorMapping} />}
+              />
+              <Route
+                path="/dashboard/pyramid"
+                element={<PrivateRoute element={Pyramid} />}
+              />
+              <Route
+                path="/dashboard/stacked"
+                element={<PrivateRoute element={Stacked} />}
+              />
             </Routes>
           </div>
         </div>
       </div>
-       
     </div>
   );
 };
